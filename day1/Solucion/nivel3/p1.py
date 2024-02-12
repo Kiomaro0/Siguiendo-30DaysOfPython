e368e3a68eac7a9ab5289cbb5e5815eb778db281
Sam <aprendesam@gmail.com>
2024-02-12 16:41:22 -0500

    Se agrego la solucion del nivel3

---
+++ day1/Solucion/nivel3/p1.py
@@ -0,0 +1,16 @@
+"""
+Write an example for different Python data types such as 
+Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+"""
+
+# Checking data types
+print("Python cuenta con los siguientes tipos de datos:\n")
+print(type(10), " Ej: 10")          # Int
+print(type(3.14), " Ej: 3.14")        # Float
+print(type(1 + 3j), " Ej: 1 + 3j")      # Complex number
+print(type('Asabeneh'), " Ej: 'Asabeneh'")  # String
+print(type([1, 2, 3]), " Ej: '[1, 2, 3]'")   # List
+print(type({'name':'Asabeneh'})," Ej: {'name':'Asabeneh'}") # Dictionary
+print(type({9.8, 3.14, 2.7}), " Ej: {9.8, 3.14, 2.7} ")    # Set
+print(type((9.8, 3.14, 2.7)) , " Ej: (9.8, 3.14, 2.7)")    # Tuple
+print(type(True), " Ej: True")    #Bool
---
+++ day1/Solucion/nivel3/p2.py
@@ -0,0 +1,5 @@
+#Find an Euclidian distance between (2, 3) and (10, 8)
+import math
+
+disEu = math.sqrt(pow((10-2),2)+pow((8-3),2))
+print("La distancia euclidana enre (2,3) y (10,8) es: ", disEu)
